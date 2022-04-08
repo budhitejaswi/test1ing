1d7b729d9d057e2c776d92335ce44193f01ca4dd
budhitejaswi <87587371+budhitejaswi@users.noreply.github.com>
2022-04-08 18:25:02 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+video = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup() {
+    canvas = createCanvas(500,300);
+    canvas.center();
+}
+
+function draw() {
+    image(video,0,0,500,300);
+}
+
+function start() {
+    object_detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.log("Model Loaded :)")
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(1);
+}
